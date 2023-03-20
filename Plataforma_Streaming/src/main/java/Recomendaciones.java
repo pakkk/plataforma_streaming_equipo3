5750abdb84bbe16587ab7fff4ad01dd61830f5f4
alvaroo611 <alvaromarmolromero@gmail.com>
2023-03-20 23:07:36 +0100

    Getter y Setter implementados

--- Plataforma_Streaming/src/main/java/Recomendaciones.java
+++ Plataforma_Streaming/src/main/java/Recomendaciones.java
@@ -1,4 +1,49 @@
 
-public class Recomendaciones {
 
+/**
+ *
+ * @author alvar
+ */
+public class Recomendaciones {
+int valoracionesPositivas;
+int valoracionesNegativas;
+int vecesVista;
+
+    public Recomendaciones(int valoracionesPositivas, int valoracionesNegativas, int vecesVista) {
+        this.valoracionesPositivas = valoracionesPositivas;
+        this.valoracionesNegativas = valoracionesNegativas;
+        this.vecesVista = vecesVista;
+    }
+
+    public int getValoracionesNegativas() {
+        return valoracionesNegativas;
+    }
+
+    public int getValoracionesPositivas() {
+        return valoracionesPositivas;
+    }
+
+    public int getVecesVista() {
+        return vecesVista;
+    }
+
+    public void setValoracionesPositivas(int valoracionesPositivas) {
+        this.valoracionesPositivas = valoracionesPositivas;
+    }
+
+    public void setValoracionesNegativas(int valoracionesNegativas) {
+        this.valoracionesNegativas = valoracionesNegativas;
+    }
+
+    public void setVecesVista(int vecesVista) {
+        this.vecesVista = vecesVista;
+    }
+
+    @Override
+    public String toString() {
+        return "Recomendaciones{" + "valoracionesPositivas=" + valoracionesPositivas + ", valoracionesNegativas=" + valoracionesNegativas + ", vecesVista=" + vecesVista + '}';
+    }
+    
+    
+    
 }
