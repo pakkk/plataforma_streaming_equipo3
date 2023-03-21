138ef872149bdb9439d5ce0fea21c2293c94dc91
JUAN SUTIL MESA <jsutmes2712@g.educaand.es>
2023-03-21 18:04:36 +0100

    Tarea005: desarrollo clase Categoria.java

--- Plataforma_Streaming/src/main/java/Categoria.java
+++ Plataforma_Streaming/src/main/java/Categoria.java
@@ -1,4 +1,79 @@
-
-public class Categoria {
-
+/**
+ * Clase que representa la categoria a la que pertenece un objeto audiovisual
+ * @author Juan Sutil
+ * */
+public class Categoria implements Comparable<Categoria>{
+	
+	/**
+	 * Nombre de la categoria
+	 * */
+	private String name;
+	
+	/**
+	 * Descripcion de la categoria
+	 * */
+	private String description;
+	
+	/**
+	 * Constructor parametrizado
+	 * @param name Nombre de la categoria
+	 * @param description Descripcion de la categoria
+	 * */
+	public Categoria(String name, String descripction) {
+		this.name = name;
+		this.description = descripction;
+	}
+	
+	/**
+	 * Asigna el valor del atributo name
+	 * @param name Nuevo nombre que se quiere asignar
+	 * */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Devuelve el valor del atributo Name
+	 * @return Nombre del objeto
+	 * */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Asigna el valor del atributo description
+	 * @param description Nueva descripcion que se quiere asignar
+	 * */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	/**
+	 * Devuelve el valor del atributo description
+	 * @return Descripcion del objeto
+	 * */
+	public String getDescription() {
+		return description;
+	}
+	
+	/**
+	 * Devuelve un string como representacion del objeto
+	 * @return String que representa al objeto
+	 * */
+	@Override
+	public String toString() {
+		return "[Name: " + name + " ; Description: " + description + "]";
+	}
+	
+	/**
+	 * Compara entre dos categorias
+	 * @param o Categoria con la se quiere comprobar
+	 * @return el valor 0 si las dos categorias son iguales; un valor menor a 0 si este nombre es menor que el nombre de o; y un valor superior a 0 si este nombre es mayor que el nombre de o.
+	 * */
+	@Override
+	public int compareTo(Categoria o) {
+		return this.name.compareTo(o.name);
+	}	
+	
+	
 }
