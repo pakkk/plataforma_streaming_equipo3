7bd4f44ac50e50533a510ac34e82a04bc4876ebd
Silvia <1DAM@1DAM-01>
2023-03-24 14:44:07 +0100

    Clase auxiliar

---
+++ Plataforma_Streaming/src/main/java/funciones_Auxiliar.java
@@ -0,0 +1,87 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+import menus.Pelicula;
+import menus.Series;
+
+public class funciones_Auxiliar {
+	
+	static int arrancarAplicacion()
+	{
+		
+		Scanner sc=new Scanner (System.in);
+		
+		int menu;
+		System.out.println("Bienvenido a PROGRAM PELIS, soy Pelinetica, la asistente de Program Pelis");
+		System.out.println("Dejame ayudarte en tu esperiencia, ¿Que te apetece gestionar? ¿Una pelicula? Pulsa 1 o ¿Mejor una serie? pulsa2 ");
+		menu=sc.nextInt();
+		sc.close();
+		
+		if((menu>0)&&(menu<3))
+		{
+			if(menu==1)
+			{
+				return  1;
+			}else 
+			{
+				return 2;
+			}
+			
+		}else
+		{
+		
+			System.out.println("Lo siento,tienes que decirme una pelicula o una serie, es importante para que pueda hacer mi trabajo");
+			return -1;
+		}
+		
+		static void gestionarPeliculas(Pelicula [] peliculas) 
+		{
+			
+		System.out.println("¡Estupendo!. Te ubico, estamos en el menu para la gestion de directores de peliculas"+
+		"para llevar a cabo cualquier accion primero tenemos que identificar la pelicula en la que quieres realizar las modifiicaciones");
+		System.out.println("Dime el titulo de la peliculan intenta ser exact@");
+		
+		Scanner sc= new Scanner (System.in);
+		String clave=sc.nextLine();
+		
+		
+		Arrays.sort(peliculas);
+		int posicion=Arrays.binarySearch(peliculas, clave);
+		System.out.println("Bien, pues dime que hago"+
+		"Si pulsas 1: Añadimos una director al catalogo de esa pelicula"+"\n"+
+		"Si pulsas 2: Te habilito un espacion de esctritura para que me digas un director y vemos si esta en esta peli en concreto"+"\n"+
+		"si pulsas 3: Modificamos los datos de algun director ya esxistente "+
+		"si pulsas 4: Elimino al director que tu me dgas");
+		
+		int menu=sc.nextInt();
+		sc.close();
+		
+		
+			
+		}
+		static void gestionarSeries(Series[] series) 
+		{
+			System.out.println("¡Estupendo!. Te ubico, estamos en el menu para la gestion de directores de series"+
+			"para llevar a cabo cualquier accion primero tenemos que identificar la serie en la que quieres realizar las modifiicaciones");
+			System.out.println("Dime el titulo de la serie, intenta ser exact@");
+				
+			Scanner sc= new Scanner (System.in);
+			String clave=sc.nextLine();
+					
+					
+			Arrays.sort(series);
+			int posicion=Arrays.binarySearch(series, clave);
+			System.out.println("Bien, pues dime que hago"+
+			"Si pulsas 1: Añadimos una director al catalogo de esa pelicula"+"\n"+
+			"Si pulsas 2: Te habilito un espacion de esctritura para que me digas un director y vemos si esta en esta peli en concreto"+"\n"+
+			"si pulsas 3: Modificamos los datos de algun director ya esxistente "+
+			"si pulsas 4: Elimino al director que tu me dgas");
+					
+			int menu=sc.nextInt();
+			sc.close();
+		
+		}
+		
+	}
+
+}
