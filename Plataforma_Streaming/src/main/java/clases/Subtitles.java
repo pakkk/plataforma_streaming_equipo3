ef88bb1b7fa208ddeb13fa264bc6d8908041ec93
MiguelRios <mriogut600@g.educaand.es>
2023-03-27 13:44:27 +0200

    Tarea004: Crear Clase Subtitulos

---
+++ Plataforma_Streaming/src/main/java/clases/Subtitles.java
@@ -0,0 +1,69 @@
+package clases;
+/**
+ * Class representing the category to which an audiovisual object belongs
+ * @author Silvia Mesa
+ * */
+enum Language {ENGLISH, FRENCH, SPANISH, CHINESE, NOTAVAILABLE}
+
+public class Subtitles implements Comparable<Subtitles> {
+	
+	/**
+	 * Subtitle language, represented as a set of translations
+	 * */
+	
+	private Language subtitleLanguage;
+	
+	/**
+	 * Parameterized constructor
+	 * @param subtitleLanguage Subtitle language
+	 * */
+	public Subtitles(Language subtitleLanguage) {
+		this.subtitleLanguage = subtitleLanguage;
+	}
+	
+	/**
+	 * Unparameterized constructor
+	 * Subtitle language not available
+	 * */
+	public Subtitles() {
+		this.subtitleLanguage = Language.NOTAVAILABLE;
+	}
+		
+	/**
+	 * Assigns the value of the subtitleLanguage attribute
+	 * @param subtitleLanguage New value for the attribute
+	 * */
+	public void setSubtitleLanguage(Language subtitleLanguage) {
+		this.subtitleLanguage = subtitleLanguage;
+	}
+	
+	/**
+	 * Returns the value of the subtitleLanguage attribute
+	 * @return Subtitle language of the object
+	 * */
+	public Language getSubtitleLanguage() {
+		return subtitleLanguage;
+	}
+	
+	/**
+	 * Returns a string as a representation of the object
+	 * @return String representing the object
+	 * */
+	@Override
+	public String toString() {
+	
+		return "Subtitle Language: " + subtitleLanguage.toString() ;
+	}
+	
+	/**
+	 * Compare between two subtitle languages
+	 * @param s Subtitle with which you want to check
+	 * @return the value 0 if the two languages are the same;
+	 * a value less than 0 if this language is less than the language of s;
+	 * and a value greater than 0 if this language is greater than the language of s.
+	 * */
+	@Override
+	public int compareTo(Subtitles s) {
+		return this.subtitleLanguage.compareTo(s.subtitleLanguage);
+	}
+}
