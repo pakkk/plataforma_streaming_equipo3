3516aec2ea9bc4e572708f768711346c0f4362d1
Alvarolopmon <alopmon563@g.educaand.es>
2023-03-27 13:21:05 +0200

    TAREA011: Creacion de la clase Menu Recomendaciones

---
+++ Plataforma_Streaming/src/main/java/menus/MenuRecomendaciones.java
@@ -0,0 +1,88 @@
+package menus;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import clases.Audiovisual;
+import clases.Recommendations;
+
+public class MenuRecomendaciones {
+	/**
+	 * add a Recomendacion
+	 * 
+	 * @param to Audiovisual with which you want to check
+	 * @return an Audiovisual with the added recomendations
+	 */
+	public Audiovisual addRecomendations(Audiovisual a)
+	{
+
+		try (Scanner sc = new Scanner(System.in))
+		{
+
+			System.out.println("--------------------");
+			System.out.print("Enter recommendatios: ");
+			String name = sc.nextLine();
+			System.out.print("Enter the description");
+			String description = sc.next();
+			Recommendations recommendations = new Recommendations(name, 0, description);
+			return a;
+		} 
+		catch (NumberFormatException e) 
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * Shows the categories that an Audiovisual has
+	 * 
+	 * @param a Audiovisual del que extraer la lista
+	 */
+	public void showRecomendatios(Audiovisual a) 
+	{
+
+		Recommendations recommendations[] = a.getrecommendatios; 
+
+		System.out.println("--------------------");
+		System.out.println("List the Recommendations: ");
+
+		for (int i = 0; i < Recommendations.length; i++)
+		{
+			System.out.println(i + 1 + ".- " + Recommendations[i].getName()+ " " + Recommendations[i].getRecomnedatios());
+		}
+
+	}
+
+	/**
+	 * Select a Recommendatios from the list
+	 * 
+	 * @param a Audiovisual from which you want to select
+	 * @return Array position where the recommendatios is located
+	 * if it doesn't find it, it returns -1
+	 */
+	public int selectCategories(Audiovisual a)
+	{
+
+		showRecommendations(a);
+
+		System.out.println("--------------------");
+		System.out.print("Enter Recommendatiost: ");
+		int option;
+		try (Scanner sc = new Scanner(System.in))
+		{
+			option = sc.nextInt();
+			if ((option > 0) && (option < a.getRecommendatios().length))
+				return option - 1;
+			else
+				return -1;
+		} 
+		catch (InputMismatchException e)
+		{
+			return -1;
+		}
+	}
+
+	}
+
+
+
