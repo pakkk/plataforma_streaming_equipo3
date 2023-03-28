9bad88c9f06e89efb077da8cbf5ea40269c33a45
alvaroo611 <alvaromarmolromero@gmail.com>
2023-03-29 00:24:50 +0200

    Tarea0018:Estadisticas de Actores

--- Plataforma_Streaming/src/main/java/clases/Audiovisual.java
+++ Plataforma_Streaming/src/main/java/clases/Audiovisual.java
@@ -19,7 +19,9 @@ public class Audiovisual implements Comparable<Audiovisual> {
     Subtitles[] Subtitulos;
 
     
-    
+    public Audiovisual() {
+    	
+    }
     public Audiovisual(String title, String originalLanguage, int yearOfCreation, int views) {
     	
     	Subtitulos = new Subtitles[0];
