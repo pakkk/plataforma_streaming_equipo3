44358ba0186f6ca7b664d3e0b55b81c7a1080b58
1DAM <smescof060@g.educaand.es>
2023-03-29 14:29:48 +0200

    TAREA019:Comparador de recomendadciones

--- Plataforma_Streaming/src/main/java/comparadores/ComparaRecomendaciones.java
+++ Plataforma_Streaming/src/main/java/comparadores/ComparaRecomendaciones.java
@@ -2,15 +2,56 @@ package comparadores;
 
 import java.util.Comparator;
 
-import clases.Audiovisual;
 
-public class ComparaRecomendaciones implements Comparator<Audiovisual>{
+import clases.Audiovisual;
+import clases.Recommendations;
+/**
+ * 
+ * @author Silvia Mesa
+ * this class is a comparator to sort an Arrays. The first Audiovisual to be the best Ratings
+ *
+ */
+
+public class ComparaRecomendaciones implements Comparator<Audiovisual>
+{
 
 	@Override
-	public int compare(Audiovisual o1, Audiovisual o2) {
+	 /**
+		 *Returns a positive number if the left Audiovisual is more bigger that Audiovisual to right.
+		 *So returns a negative number is order is reverse.
+		 *
+		 * @return int is a indicator to order
+		 * */
+	public int compare(Audiovisual o1, Audiovisual o2)
+	{
 		
-		return 0;
+		Recommendations [] A=o1.getRecommendations();
+		Recommendations [] B=o2.getRecommendations();
+		int acumA = 0;
+		int acumB=0;
+			
+		if(A.length>B.length) 
+		{
+			for(int i = 0;i<A.length;i++)
+			{
+				acumA+=A[i].getRatings();
+				acumB+=B[i].getRatings();
+			}
+		}
+		else 
+		{
+			for(int i = 0;i<B.length;i++)
+			{
+				acumA+=A[i].getRatings();
+				acumB+=B[i].getRatings();
+			}
+		
+		
+		
 	}
-	
-
+		
+		
+		return acumA-acumB;
 }
+}
+
