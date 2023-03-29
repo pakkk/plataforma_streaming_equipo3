6ad9cec4b09429e87ede8de6fb307452f6571249
MiguelRios <mriogut600@g.educaand.es>
2023-03-29 12:01:17 +0200

    enum para idioma

---
+++ Plataforma_Streaming/src/main/java/clases/enumLanguage.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author 1DAM
+ */
+public class enumLanguage extends Subtitles {
+    enum Language {ENGLISH, FRENCH, SPANISH, CHINESE, NOTAVAILABLE}
+}
