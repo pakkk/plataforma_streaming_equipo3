cb02f458c5d990d0a48dab9c459e44e51eb59ab9
1DAM <1DAM@1DAM-01>
2023-03-29 11:47:25 +0200

    Merge branch 'main' of https://github.com/pakkk/plataforma_streaming_equipo3.git into main

---
+++ Plataforma_Streaming/src/main/java/comparadores/ComparaRecomendaciones.java
@@ -0,0 +1,16 @@
+package comparadores;
+
+import java.util.Comparator;
+
+import clases.Audiovisual;
+
+public class ComparaRecomendaciones implements Comparator<Audiovisual>{
+
+	@Override
+	public int compare(Audiovisual o1, Audiovisual o2) {
+		
+		return 0;
+	}
+	
+
+}
