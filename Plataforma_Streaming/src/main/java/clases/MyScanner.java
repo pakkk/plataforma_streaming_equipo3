55eadaabcbf5fffe0b30061e834ef18cbe8f461e
JUAN SUTIL MESA <jsutmes2712@g.educaand.es>
2023-03-30 17:57:11 +0200

    Mejora scanners

---
+++ Plataforma_Streaming/src/main/java/clases/MyScanner.java
@@ -0,0 +1,18 @@
+package clases;
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jsuti
+ */
+public class MyScanner{
+    
+    static public Scanner scanner = new Scanner(System.in);
+    
+}
