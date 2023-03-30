63c93a3b74fa9f29e74af67bfd9c6769bc288c56
MiguelRios <mriogut600@g.educaand.es>
2023-03-30 18:37:23 +0200

    Tarea007: Crear Clase Serie

--- Plataforma_Streaming/src/main/java/clases/Serie.java
+++ Plataforma_Streaming/src/main/java/clases/Serie.java
@@ -2,36 +2,39 @@ package clases;
 
 /**
  * This is the class for Series. It extends the Audiovisual class and implements the Comparable interface.
- * 
  * @author Miguel
  */
-public class Serie extends Audiovisual implements Comparable<Audiovisual> {
+public class Serie extends Audiovisual implements Comparable<Audiovisual>
+{
 
     // instance variables
     private int episodes; // number of episodes in the series
     private int seasons; // number of seasons in the series
     
-
-	public Serie(String title, String originalLanguage, int yearOfCreation, int views,int episodes ,int seasons) {
-		super(title, originalLanguage, yearOfCreation, views);
-		this.episodes = episodes;
-	    this.seasons = seasons;
-		
-	}
+    public Serie(String title, String originalLanguage, int yearOfCreation, int views,int episodes ,int seasons)
+    {
+        super(title, originalLanguage, yearOfCreation, views);
+        this.episodes = episodes;
+	this.seasons = seasons;
+    }
     // getter and setter methods for the instance variables
-    public int getSeasons() {
+    public int getSeasons()
+    {
         return seasons;
     }
 
-    public void setSeasons(int seasons) {
+    public void setSeasons(int seasons)
+    {
         this.seasons = seasons;
     }
 
-    public int getEpisodes() {
+    public int getEpisodes()
+    {
         return episodes;
     }
 
-    public void setEpisodes(int episodes) {
+    public void setEpisodes(int episodes)
+    {
         this.episodes = episodes;
     }
 
@@ -41,19 +44,19 @@ public class Serie extends Audiovisual implements Comparable<Audiovisual> {
      * @return a string representation of the object
      */
     @Override
-    public String toString() {
+    public String toString()
+    {
         return super.toString(); // call the toString method of the superclass
     }
 
     /**
      * Compares two Audiovisual objects. This method is required to implement the Comparable interface.
-     * 
      * @param o the other Audiovisual object to compare to
      * @return an integer representing the comparison result
      */
     @Override
-    public int compareTo(Audiovisual o) {
+    public int compareTo(Audiovisual o)
+    {
         return super.compareTo(o); // call the compareTo method of the superclass
     }
-    
 }
