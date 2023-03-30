0a7d08d756ebac7f12900cc7b95e34b0f45e6987
Alvarolopmon <alopmon563@g.educaand.es>
2023-03-30 22:39:35 +0200

    Tarea011: Cambio de codigo

--- Plataforma_Streaming/src/main/java/menus/MenuRecomendaciones.java
+++ Plataforma_Streaming/src/main/java/menus/MenuRecomendaciones.java
@@ -1,88 +1,276 @@
 package menus;
 
 import java.util.InputMismatchException;
-import java.util.Scanner;
-
-import clases.Audiovisual;
-import clases.Recommendations;
+import clases.*;
 
 public class MenuRecomendaciones {
 	/**
 	 * add a Recomendacion
 	 * 
 	 * @param to Audiovisual with which you want to check
-	 * @return an Audiovisual with the added recomendations
+	 * @return an Audiovisual with the added Recomendacion
 	 */
-	public Audiovisual addRecomendations(Audiovisual a)
+    Audiovisual a;
+
+    public MenuRecomendaciones(Audiovisual a) {
+        this.a = a;
+    }
+        public void startMenu() {
+        int menu;
+        do {
+
+            System.out.println("----------------------------------------");
+            System.out.println("----MENU RECOMMENDATIONS----");
+            System.out.println("1.- Add a new recommendatios");
+            System.out.println("2.- Delete an recommendatios");
+            System.out.println("3.- Modify an recommendatios");
+            System.out.println("4.- Search an recommendatios");
+            System.out.println("5.- Exit");
+            
+            try{
+               
+                menu = Integer.parseInt(MyScanner.scanner.nextLine());
+            } catch (InputMismatchException ex) {
+                menu = 0;
+            }
+
+            switch (menu) {
+                case 1 -> {
+                    boolean b = addRecommendation();
+                    if (b) {
+                        System.out.println("Recommendation entered correctly");
+                    } else {
+                        System.out.println("Recommendation not introduced");
+                    }
+                }
+                case 2 -> {
+                    boolean b = deleteRecommendation();
+                    if (b) {
+                        System.out.println("Recommendation successfully deleted");
+                    } else {
+                        System.out.println("The recommendation could not be deleted");
+                    }
+                }
+                case 3 -> {
+
+                    boolean b = modifyRecommendation();
+                    if (b) {
+                        System.out.println("Recommendation modified successfully");
+                    } else {
+                        System.out.println("The Recommendation could not be modified");
+                    }
+                }
+                case 4 -> {
+                    int b = findRecommendation();
+                    if (b == 0) {
+                        System.out.println("The Recommendation could not be introduced");
+                    }
+                }
+                case 5 -> {
+                    
+                }
+                default -> {
+                    System.out.println("invalid option");
+                }
+            }
+        } while (menu != 5);
+    }
+	
+	public boolean addRecommendation()
 	{
 
-		try (Scanner sc = new Scanner(System.in))
+		try 
 		{
 
 			System.out.println("--------------------");
-			System.out.print("Enter recommendatios: ");
-			String name = sc.nextLine();
-			System.out.print("Enter the description");
-			String description = sc.next();
-			Recommendations recommendations = new Recommendations(name, 0, description);
-			return a;
+			System.out.print("Enter Recomendacion: ");
+			String name = MyScanner.scanner.nextLine();
+			
+			Recommendations Recommendations = new Recommendations(name, 0, name);
+			this.a.recommendations();
+			return true;
 		} 
-		catch (NumberFormatException e) 
+		catch (InputMismatchException e) 
 		{
-			return null;
+			return false;
 		}
 	}
 
 	/**
 	 * Shows the categories that an Audiovisual has
 	 * 
-	 * @param a Audiovisual del que extraer la lista
 	 */
-	public void showRecommendations(Audiovisual a) 
+	public void displayRecommendation() 
 	{
 
-		Recommendations recommendations[] = a.getRecommendations(); 
+		Recommendations[] Recommendations = a.getRecommendations();
 
 		System.out.println("--------------------");
-		System.out.println("List the Recommendations: ");
+		System.out.println("List the Recommendation: ");
 
-		for (int i = 0; i < recommendations.length; i++)
+		for (int i = 0; i < Recommendations.length; i++)
 		{
-			System.out.println(i + 1 + ".- " + recommendations[i].getUsername()+ " " + recommendations[i].getRatings()+"	"+recommendations[i].getComment());
+			System.out.println(i + 1 + ".- " + Recommendations[i].getComment()+ " " + Recommendations[i].getRatings());
 		}
 
 	}
 
 	/**
-	 * Select a Recommendatios from the list
+	 * Select a category from the list
 	 * 
-	 * @param a Audiovisual from which you want to select
-	 * @return Array position where the recommendatios is located
+	 * @return Array position where the category is located
 	 * if it doesn't find it, it returns -1
 	 */
-	public int selectCategories(Audiovisual a)
+	public int selectRecommendation()
 	{
 
-		showRecommendations(a);
+		displayRecommendation();
 
 		System.out.println("--------------------");
-		System.out.print("Enter Recommendatiost: ");
+		System.out.print("Enter Recommendations (number) you want to select: ");
 		int option;
-		try (Scanner sc = new Scanner(System.in))
+		try 
 		{
-			option = sc.nextInt();
+			option = Integer.parseInt(MyScanner.scanner.nextLine());
 			if ((option > 0) && (option < a.getRecommendations().length))
-				return option - 1;
+				return option -1;
 			else
 				return -1;
 		} 
-		catch (InputMismatchException e)
+		catch (NumberFormatException e)
 		{
 			return -1;
 		}
 	}
 
+	/**
+	 * Compare between two categories
+	 * 
+     * @return 
+	 */
+	public boolean modifyRecommendation() 
+	{
+		int pos=selectRecommendation();
+                if (pos != -1) {
+                    Recommendations Recommendations = this.a.getRecommendations()[pos];
+                    System.out.println("--------------------");
+                    System.out.println("""
+                                        I show you the possible modifications
+                                       1.Name
+                                       2.Description""");
+
+                    int atributte;
+
+                    try 
+                    {
+                            atributte = Integer.parseInt(MyScanner.scanner.nextLine());
+                            switch (atributte)
+                            {
+                            case 1 -> 			{
+                                    System.out.println("Enter new name");
+                                    String newText = MyScanner.scanner.nextLine();
+                                    Recommendations.setUsername(newText);
+                                    return true;
+                            }
+                            
+                            case 2 -> 			{
+                                    System.out.println("Enter new Recommendation");
+                                    String newText = MyScanner.scanner.next();
+                                    Recommendations.setComment(newText);
+                                    return true;
+                            }
+                            default->{
+                                return false;
+                            }
+                            }
+                    } 
+                    catch (NumberFormatException ex)
+                    {
+
+
+                            return false;
+                    }
+                  
+                }else{
+                    return false;
+                }
+            }
+	/**
+	 * Compare between two Recommendation
+	 * 
+        * @return 
+	 */
+	public boolean deleteRecommendation() {
+        int posicion = selectRecommendation();
+        if (posicion != -1) {
+            this.a.deleteRecommendations(posicion);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+	/**
+	 * Compare to Recommendations
+     * @return 
+	 */
+	public int findCategories()
+	{
+		System.out.println("""
+                                    I show you the possible search keys
+                                   1.Name
+                                   2.Description""");
+
+		int atributte,cant=0;
+
+		try 
+		{
+			atributte = Integer.parseInt(MyScanner.scanner.nextLine());
+			
+			switch(atributte) {
+				case 1 -> {
+					System.out.println("Enter name");
+					String newText = MyScanner.scanner.nextLine();
+					for (Recommendations d : a.getRecommendations())
+					{
+						if (d.getUsername().equals(newText))
+						{
+							System.out.println(d.toString());
+                                                        cant++;
+						}
+					
+					}
+
+				}
+				case 2 -> {
+					System.out.println("Enter Recommendation");
+					String newText = MyScanner.scanner.nextLine();
+					for (Recommendations d : a.getRecommendations())
+					{
+						if (d.getComment().contains(newText))
+						{
+							System.out.println(d.toString());
+                                                        cant++;
+						}
+					
+					}
+
+				}
+                                default->{
+                                    return -1;
+                                }
+			}
+			
+			
+					} 
+		catch (NumberFormatException ex) 
+		{
+                    return -1;
+
+                    
+		}
+                return cant;
 	}
 
-
+}
 
