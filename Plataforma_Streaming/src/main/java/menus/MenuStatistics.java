970548b6182a26b54956ae525f67bf9cc4a12c34
MiguelRios <mriogut600@g.educaand.es>
2023-03-30 19:30:26 +0200

    Tarea017: Crear Menu Estadisticas para realizar estadísticas de todas las películas y/o series dadas de alta

--- Plataforma_Streaming/src/main/java/menus/MenuStatistics.java
+++ Plataforma_Streaming/src/main/java/menus/MenuStatistics.java
@@ -5,21 +5,30 @@ import clases.Director;
 import java.util.Scanner;
 
 /**
+ * This class provides a menu for displaying statistics on a collection of Audiovisual objects.
+ * Users can choose to view all audiovisuals or filter by director.
  * 
  * @author Miguel
- * 
  */
 public class MenuStatistics {
     
+    /**
+     * The main method of the class. Initializes the Audiovisual array and displays the menu until the user chooses to exit.
+     * 
+     * @param args the command line arguments (not used)
+     */
     public static void main(String[] args) {
         Audiovisual[] audioVisuals = createAudioVisuals();
 
         boolean exit = false;
         do {
+            // Display the menu options
             System.out.println("Welcome to the statistics menu:");
             System.out.println("1. Show all audiovisuals.");
             System.out.println("2. Show audiovisuals by director.");
             System.out.println("3. Exit.");
+            
+            // Get the user's choice and execute the corresponding method
             int option = readOption();
             switch (option) {
                 case 1 -> showAllAudioVisuals(audioVisuals);
@@ -33,10 +42,20 @@ public class MenuStatistics {
         } while (!exit);
     }
 
+    /**
+     * Initializes the Audiovisual array.
+     * 
+     * @return an empty Audiovisual array with 10 elements
+     */
     private static Audiovisual[] createAudioVisuals() {
         return new Audiovisual[10];
     }
 
+    /**
+     * Prompts the user to enter a menu option and validates the input.
+     * 
+     * @return the user's chosen option as an integer
+     */
     private static int readOption() {
         boolean valid = false;
         int option = 0;
@@ -53,6 +72,11 @@ public class MenuStatistics {
         return option;
     }
 
+    /**
+     * Displays all Audiovisual objects in the array.
+     * 
+     * @param audioVisuals the Audiovisual array to display
+     */
     private static void showAllAudioVisuals(Audiovisual[] audioVisuals) {
         System.out.println("List of all audiovisuals:");
         for (Audiovisual a : audioVisuals) {
@@ -60,6 +84,11 @@ public class MenuStatistics {
         }
     }
 
+    /**
+     * Prompts the user to enter one or more director names and displays all Audiovisual objects that have at least one of those directors.
+     * 
+     * @param audioVisuals the Audiovisual array to search
+     */
     private static void searchAudioVisualsByDirectors(Audiovisual[] audioVisuals) {
         boolean stop = true;
         do {
@@ -79,7 +108,13 @@ public class MenuStatistics {
             }
         } while (!stop);
     }
-
+    
+    /**
+    This method checks whether the given audiovisual was directed by one or more of the directors whose names are provided.
+    @param a the audiovisual to check
+    @param directorNames an array of director names to search for
+    @return true if the audiovisual was directed by at least one of the directors, false otherwise
+    */
     private static boolean isDirector(Audiovisual a, String[] directorNames) {
         Director[] directors = a.getdirectors();
         for (Director director : directors) {
