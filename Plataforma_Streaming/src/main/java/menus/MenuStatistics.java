61b8dfcbc26f0314243882de393640deed814c71
MiguelRios <mriogut600@g.educaand.es>
2023-03-31 00:32:01 +0200

    Tarea022

--- Plataforma_Streaming/src/main/java/menus/MenuStatistics.java
+++ Plataforma_Streaming/src/main/java/menus/MenuStatistics.java
@@ -3,22 +3,31 @@ package menus;
 import clases.*;
 
 /**
- * This class provides a menu for displaying statistics on a collection of Audiovisual objects.
- * Users can choose to view all audiovisuals or filter by director or category.
- * 
+ * This class provides a menu for displaying statistics on a collection of
+ * Audiovisual objects. Users can choose to view all audiovisuals or filter by
+ * director or category.
+ *
  * @author Miguel
  */
 public class MenuStatistics {
-    
+
     /**
-     * Initializes the Audiovisual array and displays the menu until the user chooses to exit.
-     * 
+     * Initializes the Audiovisual array and displays the menu until the user
+     * chooses to exit.
+     *
      * @param args the command line arguments (not used)
      */
-    public static void main(String[] args) {
-        Audiovisual[] audioVisuals = createAudioVisuals();
+    Audiovisual a = null;
 
-        boolean exit = false;
+    public MenuStatistics(Audiovisual a) {
+        this.a = a;
+    }
+
+    Audiovisual[] audioVisuals;
+
+    boolean exit = false;
+
+    public void mostrarMenu() {
         do {
             // Display the menu options
             System.out.println("Welcome to the statistics menu:");
@@ -26,34 +35,29 @@ public class MenuStatistics {
             System.out.println("2. Show audiovisuals by director.");
             System.out.println("3. Show audiovisuals by category.");
             System.out.println("4. Exit.");
-            
+
             // Get the user's choice and execute the corresponding method
             int option = readOption();
             switch (option) {
-                case 1 -> showAllAudioVisuals(audioVisuals);
-                case 2 -> searchAudioVisualsByDirectors(audioVisuals);
-                case 3 -> searchAudioVisualsByCategory(audioVisuals);
+                case 1 ->
+                    showAllAudioVisuals(audioVisuals);
+                case 2 ->
+                    searchAudioVisualsByDirectors(audioVisuals);
+                case 3 ->
+                    searchAudioVisualsByCategory(audioVisuals);
                 case 4 -> {
                     System.out.println("Goodbye!");
                     exit = true;
                 }
-                default -> System.out.println("Invalid option.");
+                default ->
+                    System.out.println("Invalid option.");
             }
         } while (!exit);
     }
 
     /**
-     * Initializes the Audiovisual array.
-     * 
-     * @return an empty Audiovisual array with 10 elements
-     */
-    private static Audiovisual[] createAudioVisuals() {
-        return new Audiovisual[10];
-    }
-
-    /**
      * Prompts the user to enter a menu option and validates the input.
-     * 
+     *
      * @return the user's chosen option as an integer
      */
     private static int readOption() {
@@ -73,7 +77,7 @@ public class MenuStatistics {
 
     /**
      * Displays all Audiovisual objects in the array.
-     * 
+     *
      * @param audioVisuals the Audiovisual array to display
      */
     private static void showAllAudioVisuals(Audiovisual[] audioVisuals) {
@@ -84,8 +88,9 @@ public class MenuStatistics {
     }
 
     /**
-     * Prompts the user to enter one or more director names and displays all Audiovisual objects that have at least one of those directors.
-     * 
+     * Prompts the user to enter one or more director names and displays all
+     * Audiovisual objects that have at least one of those directors.
+     *
      * @param audioVisuals the Audiovisual array to search
      */
     private static void searchAudioVisualsByDirectors(Audiovisual[] audioVisuals) {
@@ -107,13 +112,16 @@ public class MenuStatistics {
             }
         } while (!stop);
     }
-    
+
     /**
-    This method checks whether the given audiovisual was directed by one or more of the directors whose names are provided.
-    @param a the audiovisual to check
-    @param directorNames an array of director names to search for
-    @return true if the audiovisual was directed by at least one of the directors, false otherwise
-    */
+     * This method checks whether the given audiovisual was directed by one or
+     * more of the directors whose names are provided.
+     *
+     * @param a the audiovisual to check
+     * @param directorNames an array of director names to search for
+     * @return true if the audiovisual was directed by at least one of the
+     * directors, false otherwise
+     */
     private static boolean isDirector(Audiovisual a, String[] directorNames) {
         Director[] directors = a.getdirectors();
         for (Director director : directors) {
@@ -125,24 +133,21 @@ public class MenuStatistics {
         }
         return false;
     }
-    
-    private static void searchAudioVisualsByCategory(Audiovisual[] audioVisuals)
-    {
-    System.out.print("Enter a list of categories separated by commas: ");
-    String input = MyScanner.scanner.nextLine();
-    boolean found = false;
-    for (Audiovisual a : audioVisuals)
-    {
-        for (Category c : a.getcategories())
-        {
-            if (input.contains(c.getName())){
-                System.out.println(a.toString());
-                found = true;
+
+    private static void searchAudioVisualsByCategory(Audiovisual[] audioVisuals) {
+        System.out.print("Enter a list of categories separated by commas: ");
+        String input = MyScanner.scanner.nextLine();
+        boolean found = false;
+        for (Audiovisual a : audioVisuals) {
+            for (Category c : a.getcategories()) {
+                if (input.contains(c.getName())) {
+                    System.out.println(a.toString());
+                    found = true;
+                }
+                if (!found) {
+                    System.out.println("No audiovisuals found with the specified categories.");
+                }
             }
         }
-    }
-    if (!found){
-        System.out.println("No audiovisuals found with the specified categories.");
-    }
     }
 }
