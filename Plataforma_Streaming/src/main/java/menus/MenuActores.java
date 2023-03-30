ca9e1f80bbb2d738d35bdfa5e14f8f17631f6592
unknown <jsutmes2712@g.educaand.es>
2023-03-30 08:12:10 +0200

    Tarea010

--- Plataforma_Streaming/src/main/java/menus/MenuActores.java
+++ Plataforma_Streaming/src/main/java/menus/MenuActores.java
@@ -5,26 +5,6 @@ import java.util.Scanner;
 import clases.*;
 
 public class MenuActores {
-/**<<<<<<< HEAD
-=======
-	**/
-	private Audiovisual a;
-	
-	public MenuActores(Audiovisual a) {
-		this.a = a;
-	}
-	
-	public void startMenu(){
-		
-		
-		
-		
-	}
-	
-	static boolean addActor() {
-		
-		try (Scanner sc = new Scanner(System.in)){
-//>>>>>>> branch 'main' of https://github.com/pakkk/plataforma_streaming_equipo3.git
 
 	Audiovisual audioVisual;
 
@@ -104,33 +84,21 @@ public class MenuActores {
 			System.out.print("Introduzca la edad: ");
 			int age = Integer.valueOf(sc.nextLine());
 			System.out.print("Introduzca la nacionalidad: ");
-<<<<<<< HEAD
-			String nationality= sc.nextLine();
-			Actor actor = new Actor(name, lastname, gender, age, nationality);
-			a.addsupportingActores(actor);
-=======
+
 			String nationality = sc.nextLine();
 			Actor actor = new Actor(name, lastname, gender, age, nationality);
 			this.audioVisual.addsupportingActores(actor);
->>>>>>> 98f169df0c63e43f0a0bc15cc4893fed5aca6f69
+
 			return true;
 		} catch (NumberFormatException e) {
 			return false;
 		}
 	}
-<<<<<<< HEAD
-	
-	static void displayActores() {
-		
-		Actor actores[] = a.getsupportingActores();
-		
-=======
 
 	private void displayActores() {
 
 		Actor actores[] = this.audioVisual.getsupportingActores();
 
->>>>>>> 98f169df0c63e43f0a0bc15cc4893fed5aca6f69
 		System.out.println("--------------------");
 		System.out.println("Lista de actores: ");
 
@@ -139,20 +107,6 @@ public class MenuActores {
 		}
 
 	}
-<<<<<<< HEAD
-	
-	static int selectActor() {
-		
-		displayActores(a);
-		
-		System.out.println("--------------------");
-		System.out.print("Introduzca el actor (su numero) que quiera eliminar: ");
-		int opcion;
-		try (Scanner sc = new Scanner(System.in)){
-			opcion = sc.nextInt();
-<<<<<<< HEAD
-			return a.getsupportingActores()[opcion];
-=======
 
 	private int selectActor() {
 
@@ -167,29 +121,10 @@ public class MenuActores {
 				return option - 1;
 			else
 				return -1;
->>>>>>> 98f169df0c63e43f0a0bc15cc4893fed5aca6f69
-=======
-			return opcion -1;
->>>>>>> branch 'main' of https://github.com/pakkk/plataforma_streaming_equipo3.git
 		} catch (InputMismatchException e) {
 			return -1;
 		}
 	}
-<<<<<<< HEAD
-	
-	public boolean deleteActor(Audiovisual a)
-	{
-		int posicion = selectActor(a);
-		if(posicion > -1) {
-			a.deletesupportingActores(int pos);
-			return true;
-		}else {
-			return false;
-		}
-		
-	}
-	
-=======
 
 	public boolean deleteActor() {
 		int posicion = selectActor();
@@ -201,9 +136,7 @@ public class MenuActores {
 		}
 		
 	}
->>>>>>> 98f169df0c63e43f0a0bc15cc4893fed5aca6f69
 	
-<<<<<<< HEAD
 	private boolean modifyActor() {
 		int pos = selectActor();
 		if(pos != -1) {
@@ -380,92 +313,4 @@ public class MenuActores {
 		}
 		return cant;
 	}
-
-=======
-	public void findActor()
-	{
-		System.out.println("Find an actor by: " + "1.- name\n" + "2.- lastname\n" + "3.- gender\n"
-				+ "4.- age\n" + "5.- nationality");
-
-		int atributo;
-
-		try (Scanner scanner = new Scanner(System.in)) 
-		{
-			atributo = scanner.nextInt();
-		} 
-		catch (InputMismatchException ex) 
-		{
-			System.out.println("error option");
-		}
-		
-		Actor[] actors = a.getsupportingActores();
-		switch (atributo) 
-		{
-			case 1: 
-			{
-				System.out.println("dime nombre");
-				String newText = scanner.nextLine();
-				for (Actor act : actors)
-				{
-					if (act.getName().equals(newText))
-					{
-						System.out.println(d.toString());
-					}
-				}
-				break;
-			}
-			case 2: 
-			{
-				System.out.println("dime apellido");
-				String newText = scanner.nextLine();
-				for (Actor act : actors)
-				{
-					if (act.getLastname().equals(newText))
-					{
-						System.out.println(d.toString());
-					}
-				}
-				break;
-			}
-			case 3: 
-			{
-				System.out.println("dime genero");
-				String newText = scanner.nextLine();
-				for (Actor act : actors)
-				{
-					if (act.getgender().equals(newText))
-					{
-						System.out.println(d.toString());
-					}
-				}
-			}
-			case 4: 
-			{
-				System.out.println("dime edad");
-				int newText = scanner.nextInt();
-				for (Actor act : actors)
-				{
-					if (act.getAge() == (newText)) 
-					{
-						System.out.println(d.toString());
-					}
-				}
-				break;
-			}
-			case 5:
-			{
-				System.out.println("dime nacionalidad");
-				String newText = scanner.nextLine();
-				for (Actor act : actors)
-				{
-					if (act.getNationality().equals(newText))
-					{
-						System.out.println(d.toString());
-					}
-				}
-			}
-		}
-	}
-	
->>>>>>> branch 'main' of https://github.com/pakkk/plataforma_streaming_equipo3.git
 }
