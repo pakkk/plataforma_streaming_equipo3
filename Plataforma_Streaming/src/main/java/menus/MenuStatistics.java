fe2353ad196ce36d6beff7bda21de1c5da4760ca
MiguelRios <mriogut600@g.educaand.es>
2023-03-30 19:18:04 +0200

    Tarea017: Crear Menu Estadisticas para realizar estadísticas de todas las películas y/o series dadas de alta

--- Plataforma_Streaming/src/main/java/menus/MenuStatistics.java
+++ Plataforma_Streaming/src/main/java/menus/MenuStatistics.java
@@ -1,46 +1,94 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package menus;
 
 import clases.Audiovisual;
-import clases.Films;
-
-import java.util.ArrayList;
+import clases.Director;
 import java.util.Scanner;
 
 /**
- *
- * @author 1DAM
+ * 
+ * @author Miguel
+ * 
  */
 public class MenuStatistics {
-    private static final ArrayList<Audiovisual> audiovisuales = new ArrayList<>();
-
+    
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            int opcion;
-            
-            do {
-                System.out.println("MENÚ DE ESTADÍSTICAS");
-                System.out.println("1. Añadir película");
-                System.out.println("2. Añadir serie");
-                System.out.println("3. Mostrar información de todas las películas y/o series");
-                System.out.println("4. Mostrar información de películas y/o series por director");
-                System.out.println("5. Salir");
-                
-                System.out.print("Seleccione una opción: ");
-                opcion = scanner.nextInt();
-                
-                switch(opcion) {
-                    case 1 -> addAudiovisual(new Films());
-                    case 2 -> addAudiovisual(new Film());
-                    case 3 -> showAllAudiovisuals();
-                    case 4 -> showAudiovisualsByDirector();
-                    case 5 -> System.out.println("¡Hasta luego!");
-                    default -> System.out.println("Opción no válida");
+        Audiovisual[] audioVisuals = createAudioVisuals();
+
+        boolean exit = false;
+        do {
+            System.out.println("Welcome to the statistics menu:");
+            System.out.println("1. Show all audiovisuals.");
+            System.out.println("2. Show audiovisuals by director.");
+            System.out.println("3. Exit.");
+            int option = readOption();
+            switch (option) {
+                case 1 -> showAllAudioVisuals(audioVisuals);
+                case 2 -> searchAudioVisualsByDirectors(audioVisuals);
+                case 3 -> {
+                    System.out.println("Goodbye!");
+                    exit = true;
                 }
-            } while(opcion != 5);
+                default -> System.out.println("Invalid option.");
+            }
+        } while (!exit);
+    }
+
+    private static Audiovisual[] createAudioVisuals() {
+        return new Audiovisual[10];
+    }
+
+    private static int readOption() {
+        boolean valid = false;
+        int option = 0;
+        do {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter an option: ");
+                option = scanner.nextInt();
+                valid = true;
+            } catch (Exception e) {
+                System.out.println("Invalid option.");
+            }
+        } while (!valid);
+        return option;
+    }
+
+    private static void showAllAudioVisuals(Audiovisual[] audioVisuals) {
+        System.out.println("List of all audiovisuals:");
+        for (Audiovisual a : audioVisuals) {
+            System.out.println(a.toString());
         }
+    }
+
+    private static void searchAudioVisualsByDirectors(Audiovisual[] audioVisuals) {
+        boolean stop = true;
+        do {
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.print("Enter the name of one or more directors separated by commas: ");
+                String directors = scanner.nextLine();
+                String[] directorNames = directors.split(",");
+                for (Audiovisual a : audioVisuals) {
+                    if (isDirector(a, directorNames)) {
+                        System.out.println(a.toString());
+                    }
+                }
+                stop = true;
+            } catch (Exception e) {
+                System.out.println("Invalid option.");
+                stop = false;
+            }
+        } while (!stop);
+    }
+
+    private static boolean isDirector(Audiovisual a, String[] directorNames) {
+        Director[] directors = a.getdirectors();
+        for (Director director : directors) {
+            for (String name : directorNames) {
+                if (director.getName().equals(name)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
