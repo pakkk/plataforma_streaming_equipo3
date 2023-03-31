c4d51dc0519bb2e4c78b316461e70ac6b21f7be3
unknown <jsutmes2712@g.educaand.es>
2023-03-31 08:26:16 +0200

    Redefinicion texto main

--- Plataforma_Streaming/src/main/java/main.java
+++ Plataforma_Streaming/src/main/java/main.java
@@ -1,7 +1,4 @@
 
-import java.io.IOException;
-import java.util.Scanner;
-
 import clases.*;
 import menus.*;
 
@@ -11,165 +8,168 @@ public class main {
 
         Audiovisual[] dataBase = new Audiovisual[0];
         MenuGeneralAudiovisuales mga = new MenuGeneralAudiovisuales(dataBase);
-        Audiovisual devuelto = new Audiovisual();
+        Audiovisual devuelto = null;
         boolean seguir = true;
         int menu;
-
+        System.out.println("Hola bienvenid@ a nuestra plataforma, ya sabes que estamos en PELIS PLUS.Dejanos ayudarte a mejorar tu esperiencia con nosotros. Te mostramos el menú principal");
         while (seguir) {
-            System.out.println("Hola bienvenid@ a nnuestra plataforma, ya sabes que estamos en PELIS PLUS.Dejanos ayudarte"
-                    + "a mejorar tu esperiencia con nosotros. Te mostramos el menú principal");
-            System.out.println("\r\n"
-                    + "		System.out.println(\"----------------------------------------\");\r\n"
-                    + "		System.out.println(\"----MENU GENERAL----\");\r\n"
-                    + "		System.out.println(\"1.- Acciones en Referencia a Pelicula o serie\");\r\n"
-                    + "		System.out.println(\"2.- Obtener datos estadisticos sobre nuestra filmoteca\");\r\n"
-                    + "		System.out.println(\"3.- salir y apagar");
+
+            System.out.println("----------------------------------------");
+            System.out.println("----MENU GENERAL----");
+            System.out.println("1.- Acciones en Referencia a Pelicula o serie");
+            System.out.println("2.- Obtener datos estadisticos sobre nuestra filmoteca");
+            System.out.println("3.- salir y apagar");
 
             try {
                 menu = Integer.parseInt(MyScanner.scanner.nextLine());
+            } catch (NumberFormatException e) {
+                menu = 0;
+            }
+            switch (menu) {
+                case 1 -> {
+                    boolean s1 = true;
+                    while (s1) {
+                        System.out.println("----------------------------------------");
+                        System.out.println("----MENU AUDIOVISUALES----");
+                        System.out.println("1.- Dar de alta Pelicula o serie");
+                        System.out.println("2.- Eliminar Pelicula o serie");
+                        System.out.println("3.- Buscar Pelicula o serie");
+                        System.out.println("4.- Modificar Pelicula o serie");
+                        System.out.println("5.- Salir");
 
-                switch (menu) {
-                    case 1 -> {
-                        boolean s1 = true;
-                        while (s1) {
-                            System.out.println("Bien, que hacemos con esta pelicula?");
-                            System.out.println("\r\n"
-                                    + "		System.out.println(\"----------------------------------------\");\r\n"
-                                    + "		System.out.println(\"----MENU AUDIOVISUALES----\");\r\n"
-                                    + "		System.out.println(\"1.- Dar de alta Pelicula o serie\");\r\n"
-                                    + "		System.out.println(\"2.- Eliminar Pelicula o serie\");\r\n"
-                                    + "		System.out.println(\"3.- Buscar Pelicula o serie"
-                                    + "		System.out.println(\"4.- Modificar Pelicula o serie"
-                                    + "		System.out.println(\"5.- Salir");
+                        try {
+                            menu = Integer.parseInt(MyScanner.scanner.nextLine());
+                        } catch (NumberFormatException e) {
+                            menu = 0;
+                        }
+                        switch (menu) {
+                            case 1 -> {
+                                mga.addAudiovisual();
+                            }
+                            case 2 -> {
+                                mga.deleteAudiovisual();
+                            }
+                            case 3 -> {
+                                devuelto = mga.findAudiovisual();
+                                System.out.println("-------------------------------------------------------------");
+                                System.out.println(devuelto.toString());
+                            }
+                            case 4 -> {
 
-                            try {
+                                System.out.println("----------------------------------------");
+                                System.out.println("----MENU AUDIOVISUAl----");
+                                System.out.println("1.- Actores");
+                                System.out.println("2.- Categoria");
+                                System.out.println("3.- Directores");
+                                System.out.println("4.- Recomendaciones");
+                                System.out.println("5.- Subtitulos");
+                                System.out.println("6.- Salir");
+
                                 menu = Integer.parseInt(MyScanner.scanner.nextLine());
 
                                 switch (menu) {
                                     case 1 -> {
-                                        mga.addAudiovisual();
-                                    }
-                                    case 2 -> {
-                                        mga.deleteAudiovisual();
-                                    }
-                                    case 3 -> {
-                                        devuelto = mga.findAudiovisual();
-                                        System.out.println("Veo que tienes interes en este ejemplar, tienes habilitadas varias opciones"
-                                                + "te lo voy a mostrar toda su informacion"
-                                                + "-------------------------------------------------------------");
-                                        System.out.println(devuelto.toString());
-                                    }
-                                    case 4 -> {
-                                        System.out.println("Un audiovisual se compone de lo siguiente, se puede hacer Acceso, Modificacion,Borrado y Consulta de cualquier coponente, pero primero, dime cual");
-                                        System.out.println("\r\n"
-                                                + "		System.out.println(\"----------------------------------------\");\r\n"
-                                                + "		System.out.println(\"----MENU AUDIOVISUAl----\");\r\n"
-                                                + "		System.out.println(\"1.- Actores\");\r\n"
-                                                + "		System.out.println(\"2.- Categoria\");\r\n"
-                                                + "		System.out.println(\"3.- Directores\");\r\n"
-                                                + "		System.out.println(\"4.- Recomendaciones\");\r\n"
-                                                + "		System.out.println(\"5.- Subtitulos\");\r\n"
-                                                + "		System.out.println(\"6.- Salir");
-                                        
-                                        menu = Integer.parseInt(MyScanner.scanner.nextLine());
                                         System.out.println("Sobre que audiovisual quieres hacer esta modificacion");
                                         devuelto = mga.findAudiovisual();
-
-                                        switch (menu) {
-                                            case 1 -> {
-                                                MenuActores ma = new MenuActores(devuelto);
-                                                ma.startMenu();
-                                            }
-                                            case 2 -> {
-                                                MenuCategorias mc = new MenuCategorias(devuelto);
-                                                mc.startMenu();
-                                            }
-                                            case 3 -> {
-                                                MenuDirector md = new MenuDirector(devuelto);
-                                                md.startMenu();
-                                            }
-                                            case 4 -> {
-                                                MenuRecomendaciones mrc = new MenuRecomendaciones(devuelto);
-                                                mrc.startMenu();
-                                            }
-                                            case 5 -> {
-                                                MenuSubtitulos ms = new MenuSubtitulos(devuelto);
-                                                ms.mostrarMenu();
-                                            }
-                                            case 6 ->{
-                                                s1 = false;
-                                            }
-                                            default -> {
-                                                System.out.println("Opcion Incorrecta");
-                                            }
-                                        }
-                                    }
-                                    case 5 -> {
-                                        s1 = false;
-                                    }
-                                    default ->
-                                        System.out.println("Opcion Incorrecta");
-                                }
-                            } catch (NumberFormatException e) {
-                            }
-                        }
-                    }
-                    case 2 -> {
-                        boolean s1 = true;
-                        while(s1){
-                            
-                            System.out.println("\r\n"
-                                    + "		System.out.println(\"----------------------------------------\");\r\n"
-                                    + "		System.out.println(\"----MENU ESTADISTICAS----\");\r\n"
-                                    + "		System.out.println(\"1.- Numero de audiovisuales\");\r\n"
-                                    + "		System.out.println(\"2.- Buscar audiovisuales por genero\");\r\n"
-                                    + "		System.out.println(\"3.- Buscar audiovisuales por directores\");\r\n"
-                                    + "		System.out.println(\"4.- Buscar audiovisuales por actores\");\r\n"
-                                    + "		System.out.println(\"5.- Buscar audiovisuales por subtitulos\");\r\n"
-                                    + "		System.out.println(\"6.- Mejores recomendaciones\");\r\n"
-                                    + "		System.out.println(\"7.- Peores recomendaciones\");\r\n"
-                                    + "		System.out.println(\"8.- Salir");
-                            
-                            try {
-                                menu = Integer.parseInt(MyScanner.scanner.nextLine());
-                                Statistics st = new Statistics();
-                                switch (menu) {
-                                    case 1 -> {
-                                        
+                                        MenuActores ma = new MenuActores(devuelto);
+                                        ma.startMenu();
                                     }
                                     case 2 -> {
-                                        
+                                        System.out.println("Sobre que audiovisual quieres hacer esta modificacion");
+                                        devuelto = mga.findAudiovisual();
+                                        MenuCategorias mc = new MenuCategorias(devuelto);
+                                        mc.startMenu();
                                     }
                                     case 3 -> {
-                                        
+                                        System.out.println("Sobre que audiovisual quieres hacer esta modificacion");
+                                        devuelto = mga.findAudiovisual();
+                                        MenuDirector md = new MenuDirector(devuelto);
+                                        md.startMenu();
                                     }
                                     case 4 -> {
+                                        System.out.println("Sobre que audiovisual quieres hacer esta modificacion");
+                                        devuelto = mga.findAudiovisual();
+                                        MenuRecomendaciones mrc = new MenuRecomendaciones(devuelto);
+                                        mrc.startMenu();
                                     }
                                     case 5 -> {
+                                        System.out.println("Sobre que audiovisual quieres hacer esta modificacion");
+                                        devuelto = mga.findAudiovisual();
+                                        MenuSubtitulos ms = new MenuSubtitulos(devuelto);
+                                        ms.mostrarMenu();
                                     }
                                     case 6 -> {
-                                    }
-                                    case 7 -> {
-                                    }
-                                    case 8 -> {
                                         s1 = false;
                                     }
-                                    default -> System.out.println("Opcion incorrecta");
+                                    default -> {
+                                        System.out.println("Opcion Incorrecta");
+                                    }
                                 }
-                                
-                            } catch (NumberFormatException e) {
                             }
+                            case 5 -> {
+                                s1 = false;
+                            }
+                            default ->
+                                System.out.println("Opcion Incorrecta");
                         }
                     }
-                    case 3 -> {
-                        System.out.println("Adios");
-                        seguir = false;
-                    }
-                    default ->
-                        System.out.println("Opcion incorrecta");
                 }
-            } catch (NumberFormatException e) {
+                case 2 -> {
+                    boolean s1 = true;
+                    while (s1) {
+
+                        System.out.println("----------------------------------------");
+                        System.out.println("----MENU ESTADISTICAS----");
+                        System.out.println("1.- Numero de audiovisuales");
+                        System.out.println("2.- Buscar audiovisuales por genero");
+                        System.out.println("3.- Buscar audiovisuales por directores");
+                        System.out.println("4.- Buscar audiovisuales por actores");
+                        System.out.println("5.- Buscar audiovisuales por subtitulos");
+                        System.out.println("6.- Mejores recomendaciones");
+                        System.out.println("7.- Peores recomendaciones");
+                        System.out.println("8.- Salir");
+
+                        try {
+                            menu = Integer.parseInt(MyScanner.scanner.nextLine());
+                            Statistics st = new Statistics();
+                            switch (menu) {
+                                case 1 -> {
+                                    System.out.println("Existen " + dataBase.length + " audiovisuales distintos en nuestra plataforma;");
+                                }
+                                case 2 -> {
+
+                                }
+                                case 3 -> {
+
+                                }
+                                case 4 -> {
+                                }
+                                case 5 -> {
+                                }
+                                case 6 -> {
+                                }
+                                case 7 -> {
+                                }
+                                case 8 -> {
+                                    s1 = false;
+                                }
+                                default ->
+                                    System.out.println("Opcion incorrecta");
+                            }
+
+                        } catch (NumberFormatException e) {
+                        }
+                    }
+                }
+                case 3 -> {
+                    System.out.println("Adios");
+                    seguir = false;
+                }
+                default ->
+                    System.out.println("Opcion incorrecta");
             }
+
         }
+        MyScanner.scanner.close();
     }
 }
