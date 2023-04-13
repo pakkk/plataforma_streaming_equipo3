dfe302a3f0366242032e05d55ee9e86681522a55
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-04-13 21:49:06 +0200

    Clase statistics refactorizada por Pablo Elías Ruiz Cánovas

--- Plataforma_Streaming/src/main/java/es/iesjandula/plataforma_streaming/clases/Statistics.java
+++ Plataforma_Streaming/src/main/java/es/iesjandula/plataforma_streaming/clases/Statistics.java
@@ -1,35 +1,39 @@
 package es.iesjandula.plataforma_streaming.clases;
-
+import java.util.InputMismatchException;
 /**
  * This class save the statistics of the system
  * @author Alvaro Marmol
  * @author Alvaro Lopez
  * @author Miguel
  */
-
-import java.util.InputMismatchException;
-
-
-public class Statistics {
+public class Statistics 
+{
     /**
      * Enter by console name the actor and show the films the this actor
      * @param audioVisuals Arrays class Audiovisual
      */
-    public void Actors(Audiovisual[] audioVisuals) {
+    public void actors(Audiovisual[] audioVisuals) 
+    {
 
         String name = "";
         boolean stop = true;
-        do {
-            try{
+        do 
+        {
+            try
+            {
                 name = MyScanner.scanner.nextLine();
                 stop = false;
-            } catch (InputMismatchException e) {
+            } catch (InputMismatchException e) 
+            {
                 System.out.println("Invalid name");
             }
-        } while (stop);
+        } 
+        while (stop);
 
-        for (Audiovisual a : audioVisuals) {
-            if (isActor(a, name)) {
+        for (Audiovisual a : audioVisuals) 
+        {
+            if (isActor(a, name)) 
+            {
                 System.out.println(a.toString());
             }
         }
@@ -41,11 +45,14 @@ public class Statistics {
      * @return boolean true or false
      */
 
-    private boolean isActor(Audiovisual a, String name) {
-        Actor[] act = a.getSupportingActores();
-        for (Actor actor : act) {
+    private boolean isActor(Audiovisual audiovisual, String name) 
+    {
+        Actor[] actor = audiovisual.getSupportingActores();
+        for (Actor ForActor : actor) 
+        {
 
-            if (actor.getName().equals(name)) {
+            if (ForActor.getName().equals(name)) 
+            {
                 return true;
             }
         }
@@ -56,21 +63,27 @@ public class Statistics {
      * Enter by console name the Genders and show the films the this Genders
      * @param audioVisuals Arrays class Audiovisual
      */
-    public void Genders (Audiovisual[] audioVisuals) {
+    public void genders (Audiovisual[] audioVisuals) 
+    {
 
         String gender = "";
         boolean stop = true;
-        do {
-            try { 
+        do 
+        {
+            try 
+            { 
             	gender = MyScanner.scanner.nextLine();
                 stop = false;
             } catch (InputMismatchException e) {
                 System.out.println("Invalid name");
             }
-        } while (stop);
+        } 
+        while (stop);
 
-        for (Audiovisual a : audioVisuals) {
-            if (isGender(a, gender)) {
+        for (Audiovisual a : audioVisuals) 
+        {
+            if (isGender(a, gender)) 
+            {
                 System.out.println(a.toString());
             }
         }
@@ -82,11 +95,14 @@ public class Statistics {
      * @param name String Gender
      */
 
-    private boolean isGender (Audiovisual a, String Categorie) {
-        Category[] act = a.getcategories();
-        for (Category Gender : act) {
+    private boolean isGender (Audiovisual a, String categories) 
+    {
+        Category[] category = a.getcategories();
+        for (Category Gender : category) 
+        {
 
-            if (Gender.getName().equals(Categorie)) {
+            if (Gender.getName().equals(categories)) 
+            {
                 return true;
             }
         }
@@ -98,21 +114,27 @@ public class Statistics {
      * @param audioVisuals Arrays class Audiovisual
      */
     
-    public void Subtitles (Audiovisual[] audioVisuals) {
+    public void subtitles (Audiovisual[] audioVisuals) 
+    {
 
         String subtitles = "";
         boolean stop = true;
-        do {
-            try  {
+        do 
+        {
+            try  
+            {
             	subtitles = MyScanner.scanner.nextLine();
                 stop = false;
-            } catch (InputMismatchException e) {
+            } catch (InputMismatchException e) 
+            {
                 System.out.println("Invalid subtitle");
             }
         } while (stop);
         
-        for (Audiovisual a : audioVisuals) {
-            if (isSubtitles(a, subtitles)) {
+        for (Audiovisual a : audioVisuals) 
+        {
+            if (isSubtitles(a, subtitles)) 
+            {
                 System.out.println(a.toString());
             }
         }
@@ -124,11 +146,14 @@ public class Statistics {
      * @param name String Subtitles.
      */
 
-    private boolean isSubtitles (Audiovisual a, String LanguajeSubtitle) {
+    private boolean isSubtitles (Audiovisual a, String LanguajeSubtitle) 
+    {
     	Subtitles[] act = a.getSubtitles();
-        for (Subtitles subtitles : act) {
+        for (Subtitles subtitles : act) 
+        {
 
-            if (subtitles.getSubtitleLanguage().equals(LanguajeSubtitle)){
+            if (subtitles.getSubtitleLanguage().equals(LanguajeSubtitle))
+            {
                 return true;
             }
         }
@@ -141,20 +166,26 @@ public class Statistics {
      *
      * @param audioVisuals the Audiovisual array to search
      */
-    public void searchByDirectors(Audiovisual[] audioVisuals) {
+    public void searchByDirectors(Audiovisual[] audioVisuals) 
+    {
         String name = "";
         boolean stop = true;
-        do {
-            try{
+        do 
+        {
+            try
+            {
                 name = MyScanner.scanner.nextLine();
                 stop = false;
-            } catch (InputMismatchException e) {
+            } catch (InputMismatchException e) 
+            {
                 System.out.println("Invalid name");
             }
         } while (stop);
 
-        for (Audiovisual a : audioVisuals) {
-            if (isDirector(a, name)) {
+        for (Audiovisual a : audioVisuals) 
+        {
+            if (isDirector(a, name)) 
+            {
                 System.out.println(a.toString());
             }
         }
@@ -169,10 +200,13 @@ public class Statistics {
      * @return true if the audiovisual was directed by at least one of the
      * directors, false otherwise
      */
-    private static boolean isDirector(Audiovisual a, String directorNames) {
+    private static boolean isDirector(Audiovisual a, String directorNames) 
+    {
         Director[] dir = a.getdirectors();
-        for (Director director : dir) {
-            if (director.getName().equals(directorNames)) {
+        for (Director director : dir) 
+        {
+            if (director.getName().equals(directorNames)) 
+            {
                 return true;
             }
         }
