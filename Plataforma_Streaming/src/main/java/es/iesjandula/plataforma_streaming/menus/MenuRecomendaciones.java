9bcf1f8227d23bc60fb713f8e42002441a6de127
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-04-17 08:51:03 +0200

    Clase menu recomendaciones refactorizada por Pablo Elías Ruiz

--- Plataforma_Streaming/src/main/java/es/iesjandula/plataforma_streaming/menus/MenuRecomendaciones.java
+++ Plataforma_Streaming/src/main/java/es/iesjandula/plataforma_streaming/menus/MenuRecomendaciones.java
@@ -1,25 +1,35 @@
 package es.iesjandula.plataforma_streaming.menus;
 
 import java.util.InputMismatchException;
-import es.iesjandula.plataforma_streaming.clases.*;
+import es.iesjandula.plataforma_streaming.clases.Audiovisual;
+import es.iesjandula.plataforma_streaming.clases.Recommendations;
+import es.iesjandula.plataforma_streaming.clases.MyScanner;
+/**
+ * 
+ * @author Alvaro Lopez Montero
+ * This class takes care of add, modify delete and search recommendation in aaudiovisual
+ */
+public class MenuRecomendaciones 
+{
 
-public class MenuRecomendaciones {
-
+    //Audiovisual that contains recomemndations
+    private Audiovisual audiovisual;
     /**
      * add a Recomendacion
      *
      * @param to Audiovisual with which you want to check
-     * @return an Audiovisual with the added Recomendacion
+     * @return an Audiovisual with the added recommendation
      */
-    Audiovisual a;
-
-    public MenuRecomendaciones(Audiovisual a) {
-        this.a = a;
+    public MenuRecomendaciones(Audiovisual audiovisual) 
+    {
+        this.audiovisual = audiovisual;
     }
 
-    public void startMenu() {
+    public void startMenu() 
+    {
         int menu;
-        do {
+        do 
+        {
 
             System.out.println("----------------------------------------");
             System.out.println("----MENU RECOMMENDATIONS----");
@@ -29,58 +39,76 @@ public class MenuRecomendaciones {
             System.out.println("4.- Search an recommendatios");
             System.out.println("5.- Exit");
 
-            try {
+            try 
+            {
 
                 menu = Integer.parseInt(MyScanner.scanner.nextLine());
             } catch (InputMismatchException ex) {
                 menu = 0;
             }
 
-            switch (menu) {
-                case 1 -> {
+            switch (menu) 
+            {
+                case 1 -> 
+                {
                     boolean b = addRecommendation();
-                    if (b) {
+                    if (b) 
+                    {
                         System.out.println("Recommendation entered correctly");
-                    } else {
+                    } else 
+                    {
                         System.out.println("Recommendation not introduced");
                     }
                 }
-                case 2 -> {
+                case 2 -> 
+                {
                     boolean b = deleteRecommendation();
-                    if (b) {
+                    if (b) 
+                    {
                         System.out.println("Recommendation successfully deleted");
                     } else {
-                        System.out.println("The recommendation could not be deleted");
-                    }
+}
                 }
-                case 3 -> {
+                case 3 -> 
+                {
 
                     boolean b = modifyRecommendation();
-                    if (b) {
+                    if (b) 
+                    {
                         System.out.println("Recommendation modified successfully");
-                    } else {
+                    } else 
+                    {
                         System.out.println("The Recommendation could not be modified");
                     }
                 }
-                case 4 -> {
+                case 4 -> 
+                {
                     int b = findRecommendation();
-                    if (b == 0) {
+                    if (b == 0) 
+                    {
                         System.out.println("The Recommendation could not be introduced");
                     }
                 }
-                case 5 -> {
+                case 5 -> 
+                {
 
                 }
-                default -> {
+                default -> 
+                {
                     System.out.println("invalid option");
                 }
             }
         } while (menu != 5);
     }
+    /**
+     * This method add a recommmendation in a audiovisual
+     * @return the boolean that confirm his insert
+     */
+    public boolean addRecommendation() 
+    {
 
-    public boolean addRecommendation() {
-
-        try {
+        try 
+        {
 
             System.out.println("--------------------");
             System.out.print("Enter username: ");
@@ -91,9 +119,10 @@ public class MenuRecomendaciones {
             String comment = MyScanner.scanner.nextLine();
 
             Recommendations Recommendations = new Recommendations(name, ratings, comment);
-            this.a.addRecommendations(Recommendations);
+            this.audiovisual.addRecommendations(Recommendations);
             return true;
-        } catch (InputMismatchException e) {
+        } catch (InputMismatchException e) 
+        {
             return false;
         }
     }
@@ -102,14 +131,16 @@ public class MenuRecomendaciones {
      * Shows the categories that an Audiovisual has
      *
      */
-    public void displayRecommendation() {
+    public void displayRecommendation() 
+    {
 
-        Recommendations[] Recommendations = a.getRecommendations();
+        Recommendations[] Recommendations = this.audiovisual.getRecommendations();
 
         System.out.println("--------------------");
         System.out.println("List the Recommendation: ");
 
-        for (int i = 0; i < Recommendations.length; i++) {
+        for (int i = 0; i < Recommendations.length; i++) 
+        {
             System.out.println(i + 1 + ".- " + Recommendations[i].toString());
         }
 
@@ -121,21 +152,25 @@ public class MenuRecomendaciones {
      * @return Array position where the category is located if it doesn't find
      * it, it returns -1
      */
-    public int selectRecommendation() {
+    public int selectRecommendation() 
+    {
 
         displayRecommendation();
 
         System.out.println("--------------------");
         System.out.print("Enter Recommendations (number) you want to select: ");
         int option;
-        try {
+        try 
+        {
             option = Integer.parseInt(MyScanner.scanner.nextLine());
-            if ((option > 0) && (option < a.getRecommendations().length)) {
+            if ((option > 0) && (option < this.audiovisual.getRecommendations().length)) 
+            {
                 return option - 1;
             } else {
                 return -1;
             }
-        } catch (NumberFormatException e) {
+        } catch (NumberFormatException e) 
+        {
             return -1;
         }
     }
@@ -145,10 +180,12 @@ public class MenuRecomendaciones {
      *
      * @return
      */
-    public boolean modifyRecommendation() {
+    public boolean modifyRecommendation() 
+    {
         int pos = selectRecommendation();
-        if (pos != -1) {
-            Recommendations Recommendations = this.a.getRecommendations()[pos];
+        if (pos != -1) 
+        {
+            Recommendations Recommendations = this.audiovisual.getRecommendations()[pos];
             System.out.println("--------------------");
             System.out.println("""
                                         I show you the possible modifications
@@ -158,37 +195,45 @@ public class MenuRecomendaciones {
 
             int atributte;
 
-            try {
+            try 
+            {
                 atributte = Integer.parseInt(MyScanner.scanner.nextLine());
-                switch (atributte) {
-                    case 1 -> {
+                switch (atributte) 
+                {
+                    case 1 -> 
+                    {
                         System.out.println("Enter new name");
                         String newText = MyScanner.scanner.nextLine();
                         Recommendations.setUsername(newText);
                         return true;
                     }
 
-                    case 2 -> {
+                    case 2 -> 
+                    {
                         System.out.println("Enter new description");
                         String newText = MyScanner.scanner.nextLine();
                         Recommendations.setComment(newText);
                         return true;
                     }
-                    case 3 -> {
+                    case 3 -> 
+                    {
                         System.out.println("Enter new Recommendation");
                         int newInt = Integer.parseInt(MyScanner.scanner.nextLine());
                         Recommendations.setRatings(newInt);
                         return true;
                     }
-                    default -> {
+                    default -> 
+                    {
                         return false;
                     }
                 }
-            } catch (NumberFormatException ex) {
+            } catch (NumberFormatException ex) 
+            {
                 return false;
             }
 
-        } else {
+        } else 
+        {
             return false;
         }
     }
@@ -198,12 +243,15 @@ public class MenuRecomendaciones {
      *
      * @return
      */
-    public boolean deleteRecommendation() {
+    public boolean deleteRecommendation() 
+    {
         int posicion = selectRecommendation();
-        if (posicion != -1) {
-            this.a.deleteRecommendations(posicion);
+        if (posicion != -1) 
+        {
+            this.audiovisual.deleteRecommendations(posicion);
             return true;
-        } else {
+        } else 
+        {
             return false;
         }
 
@@ -214,7 +262,8 @@ public class MenuRecomendaciones {
      *
      * @return
      */
-    public int findRecommendation() {
+    public int findRecommendation() 
+    {
         System.out.println("""
                                     I show you the possible search keys
                                    1.Name
@@ -223,15 +272,20 @@ public class MenuRecomendaciones {
 
         int atributte, cant = 0;
 
-        try {
+        try 
+        {
             atributte = Integer.parseInt(MyScanner.scanner.nextLine());
 
-            switch (atributte) {
-                case 1 -> {
+            switch (atributte) 
+            {
+                case 1 -> 
+                {
                     System.out.println("Enter name");
                     String newText = MyScanner.scanner.nextLine();
-                    for (Recommendations d : a.getRecommendations()) {
-                        if (d.getUsername().equals(newText)) {
+                    for (Recommendations d : this.audiovisual.getRecommendations()) 
+                    {
+                        if (d.getUsername().equals(newText)) 
+                        {
                             System.out.println(d.toString());
                             cant++;
                         }
@@ -239,11 +293,14 @@ public class MenuRecomendaciones {
                     }
 
                 }
-                case 2 -> {
+                case 2 -> 
+                {                	
                     System.out.println("Enter description");
                     String newText = MyScanner.scanner.nextLine();
-                    for (Recommendations d : a.getRecommendations()) {
-                        if (d.getComment().contains(newText)) {
+                    for (Recommendations d : this.audiovisual.getRecommendations())
+                    {
+                        if (d.getComment().contains(newText)) 
+                        {
                             System.out.println(d.toString());
                             cant++;
                         }
@@ -251,11 +308,14 @@ public class MenuRecomendaciones {
                     }
 
                 }
-                case 3 -> {
+                case 3 -> 
+                {
                     System.out.println("Enter Rating");
                     int newInt = Integer.parseInt(MyScanner.scanner.nextLine());
-                    for (Recommendations d : a.getRecommendations()) {
-                        if (d.getRatings() == newInt) {
+                    for (Recommendations d : this.audiovisual.getRecommendations()) 
+                    {
+                        if (d.getRatings() == newInt) 
+                        {
                             System.out.println(d.toString());
                             cant++;
                         }
@@ -263,12 +323,14 @@ public class MenuRecomendaciones {
                     }
 
                 }
-                default -> {
+                default -> 
+                {
                     return -1;
                 }
             }
 
-        } catch (NumberFormatException ex) {
+        } catch (NumberFormatException ex) 
+        {
             return -1;
 
         }
